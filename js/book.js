4c72cd508751402f770fe137537b29c77b11f269
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2023-09-21 10:30:17 -0400

    Array of book objects. Card Design

--- js/book.js
+++ js/book.js
@@ -1,11 +1,18 @@
-const btn_add = document.querySelector(".add-book");
+const btn_addBook = document.querySelector(".add-book");
 /**@type {HTMLElement} */
 const modal = document.querySelector(".modal");
-const addBook = document.querySelector(".add-book");
 const form = document.querySelector("form");
+const toggle = document.querySelectorAll(".fa-toggle-on");
+const btn_addToLibrary = document.querySelector(".add-library");
+
+//BOOK Element Variables
+const myLibrary = [];
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
 
 //MODAL Window
-btn_add.addEventListener(
+btn_addBook.addEventListener(
   "click",
   () => {
     modal.style.visibility = "visible";
@@ -16,7 +23,7 @@ btn_add.addEventListener(
 
 modal.addEventListener("click", (e) => {
   const isOutside = !e.target.closest("form");
-  console.log(isOutside);
+  //   console.log(isOutside);
   if (isOutside) {
     closeModal();
   }
@@ -25,3 +32,24 @@ modal.addEventListener("click", (e) => {
 function closeModal() {
   modal.style.visibility = "hidden";
 }
+
+//TOGGLE
+toggle.forEach((tog) => {
+  tog.addEventListener("click", () => {
+    tog.classList.toggle("off");
+  });
+});
+
+//BOOK OBJECT
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  myLibrary.push(new Book(title.value, author.value, pages.value));
+  console.log(myLibrary);
+}
+
+btn_addToLibrary.addEventListener("click", addBookToLibrary);
