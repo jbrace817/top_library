f1e4782f1147260baa6b6ce23d4911bc3f1243b7
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2023-09-22 09:50:08 -0400

    Create object. Remove Object. Toggle Switch

--- js/book.js
+++ js/book.js
@@ -2,14 +2,19 @@ const btn_addBook = document.querySelector(".add-book");
 /**@type {HTMLElement} */
 const modal = document.querySelector(".modal");
 const form = document.querySelector("form");
-const toggle = document.querySelectorAll(".fa-toggle-on");
+
 const btn_addToLibrary = document.querySelector(".add-library");
 
-//BOOK Element Variables
+//LIBRARY Array
 const myLibrary = [];
+
+//BOOK Object Variables
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
+
+//CARD
+const cardContainer = document.querySelector(".container");
 
 //MODAL Window
 btn_addBook.addEventListener(
@@ -34,11 +39,14 @@ function closeModal() {
 }
 
 //TOGGLE
-toggle.forEach((tog) => {
-  tog.addEventListener("click", () => {
-    tog.classList.toggle("off");
+function toggleSwitch() {
+  const toggle = document.querySelectorAll(".fa-toggle-on");
+  toggle.forEach((tog) => {
+    tog.addEventListener("click", () => {
+      tog.classList.toggle("off");
+    });
   });
-});
+}
 
 //BOOK OBJECT
 function Book(title, author, pages) {
@@ -47,9 +55,60 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
+//ADD TO LIBRARY
+
+function createCard(index) {
+  const cardContent = `
+  <div class="card-container" id="${index}">
+        <div class="card" >
+          <p class="card-left">Title:</p>
+          <p class="card-right">
+            "${myLibrary[index].title}"
+          </p>
+          <p class="card-left">Author:</p>
+          <p class="card-right">${myLibrary[index].author}</p>
+          <p class="card-left">Pages:</p>
+          <p class="card-right">${myLibrary[index].pages}</p>
+          <p class="card-left center">Read:</p>
+          <i class="fa-solid fa-toggle-on fa-2x"></i>
+          <div class="card-bottom">
+            <div class="trash">
+              <i class="fa-solid fa-trash-can fa-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+  return (cardContainer.innerHTML += cardContent);
+}
+
 function addBookToLibrary() {
-  myLibrary.push(new Book(title.value, author.value, pages.value));
-  console.log(myLibrary);
+  const newBook = myLibrary.push(
+    new Book(title.value, author.value, pages.value)
+  );
+  let index = myLibrary.length - 1;
+  console.log(index);
+  createCard(index);
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  modal.style.visibility = "hidden";
+  toggleSwitch();
+  removeCard();
 }
 
 btn_addToLibrary.addEventListener("click", addBookToLibrary);
+
+//REMOVE FROM LIBRARY
+function removeCard() {
+  const btn_trash = document.querySelectorAll(".fa-trash-can");
+  for (let i = 0; i < btn_trash.length; i++) {
+    const trash = btn_trash[i];
+    trash.addEventListener("click", (e) => {
+      const targetCard = e.target.closest(".card-container").id;
+      const cardComponent = document.getElementById(targetCard);
+      cardComponent.remove();
+      myLibrary.splice(targetCard, 1);
+    });
+  }
+}
